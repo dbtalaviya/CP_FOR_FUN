fb68e11ff7ffa4f6b067fb7477ff119f38c04428
Darshak Talaviya <104221798+dbtalaviya@users.noreply.github.com>
2022-08-29 23:16:42 +0530

    pattern printing c++

---
+++ db.cpp
@@ -0,0 +1,100 @@
+//here is code for printing pattern in c++
+
+/*
+
+    * * * * * * * * * * 
+   * * * * *   * * * * * 
+  * * * * *     * * * * * 
+ * * * * *       * * * * * 
+* * * * *         * * * * * 
+ * * * * *       * * * * * 
+  * * * * *     * * * * * 
+   * * * * *   * * * * * 
+    * * * * * * * * * *
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=n;j++)
+        {
+            cout<<"* ";
+        }
+
+        for(int j=1;j<i;j++)
+        {
+            cout<<" ";
+        }
+         for(int j=1;j<i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=n;j++)
+        {
+            cout<<"* ";
+        }
+
+        cout<<endl;
+    }
+        for(int i=1;i<n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=n;j++)
+        {
+            cout<<"* ";
+        }
+
+        for(int j=1;j<n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=n;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+}
+
+/*
+     * * * * * 
+    * * * * * 
+   * * * * * 
+  * * * * * 
+ * * * * *
+ */
+
+
+
+
+
+
+
+                                   
+
+
+
+
+
+
+
+
+
+
